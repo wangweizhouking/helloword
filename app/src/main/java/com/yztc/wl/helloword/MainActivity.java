674a1cfa4a6a13a8022e428d7b84bc277e22caea
wangweizhou1 <764676986@qq.com>
2016-08-24 17:09:01 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/yztc/wl/helloword/MainActivity.java

--- app/src/main/java/com/yztc/wl/helloword/MainActivity.java
+++ app/src/main/java/com/yztc/wl/helloword/MainActivity.java
@@ -1,23 +1,9 @@
 package com.yztc.wl.helloword;
 
 
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-    }
-
-    @Nullable
-    @Override
-    public View onCreatePanelView(int featureId) {
-        return super.onCreatePanelView(featureId);
-    }
 }
